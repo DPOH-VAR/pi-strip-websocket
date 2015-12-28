4ca5fcbb6a3993291b80568690ee8637c245a993
AVakhterov <dpohvar@gmail.com>
2015-12-28 09:53:31 +0500

    fixes

--- pwm-strip-controller.js
+++ pwm-strip-controller.js
@@ -5,23 +5,22 @@ const device = "/dev/pigpio";
 
 module.exports = PWMService;
 
-function PWMService(){
+function PWMService(pins){
     this.pins = pins;
     process.on('SIGINT', function() {
         this.send(pins.map(zero));
         process.nextTick(exit());
-    });
+    }.bind(this));
 };
 
 PWMService.prototype.send = function(buffer){
+	function setPWMCommand(pin,i){
+		return "p " + pin + " " + (buffer[i]||0);
+	}
     var cmd = this.pins
         .map(setPWMCommand)
         .join(" ") + "\n";
     fs.appendFileSync(device,cmd);
-}
-
-function setPWMCommand(pin,i){
-    return "p " + pin + " " + (buffer[i]||0);
 }
 
 function zero(){
